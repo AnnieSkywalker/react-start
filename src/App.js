b5cd61f4a89eda6384ff023f924a1e3511f10372
AnnieSkywalker <annie.skywalker@yandex.ru>
2024-08-06 15:15:10 +0300

    реализовано удаление поста

--- src/App.js
+++ src/App.js
@@ -15,15 +15,17 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+  }
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
   }
 
 
   return (
     <div className="App">
       <PostForm create={createPost}></PostForm>
-
-      <PostList posts={posts} title='Список постов'/>
+      <PostList remove={removePost} posts={posts} title='Список постов'/>
 
     </div>
   );
