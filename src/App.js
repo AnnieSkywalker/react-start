0c5ab268e7da863a9749fefbff2f9a8eaa042fcf
AnnieSkywalker <annie.skywalker@yandex.ru>
2024-08-13 12:13:38 +0300

    api and loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 import PostForm from "./components/PostForm";
-
-
 import './stales/App.css'
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
@@ -9,23 +7,32 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 
 import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
 
-  const [posts, setPosts] = useState([
-      {id: 1, title: 'javascript', body: 'discription' },
-      {id: 2, title: 'javascript2', body: 'discription' },
-      {id: 3, title: 'javascript3', body: 'discription' },
-    ]
-  )
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+  useEffect(()=>{
+    fetchPost();
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false);
+  }
+
+  async function fetchPost () {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
+
   }
 
   const removePost = (post) => {
@@ -47,7 +54,7 @@ function App() {
         setFilter={setFilter}
       ></PostFilter>
 
-      <PostList remove={removePost} posts={sortedAndSearchPosts} title='Список постов'/>
+      {isPostsLoading ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div> : <PostList remove={removePost} posts={sortedAndSearchPosts} title='Список постов'/>}
 
       <MyModal visible={modal} setVisible={setModal}>
           <PostForm create={createPost}></PostForm>
