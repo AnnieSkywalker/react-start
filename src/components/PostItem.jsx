ff4cd33e181f4ba4ddf5a3fede6b598cf9485963
AnnieSkywalker <annie.skywalker@yandex.ru>
2024-08-15 13:39:11 +0300

    added pagination

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -7,7 +7,7 @@ const PostItem = function (props) {
     <div className="post">
 
       <div className="post__content">
-        <strong>{props.number}. {props.post.title}</strong>
+        <strong>{props.post.id}. {props.post.title}</strong>
         <div>
           {props.post.body}
         </div>
@@ -15,7 +15,7 @@ const PostItem = function (props) {
       <div className="post__btn">
         <MyButton onClick = {() => props.remove(props.post)}>Удалить</MyButton>
       </div>
-      
+
     </div>
   )
 }
