61d65140d54175e44494a072b2f7ddc34e165208
Spiridonov <annie.skywalker@yandex.ru>
2024-11-18 15:16:19 +0300

    react Link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route,Link, Routes } from 'react-router-dom';
 import About from './Pages/About';
 import Posts from './Pages/Posts';
 
 
 function App() {
-	return <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/about' element={<About />}>
-        </Route>
-        <Route path='/posts' element={<Posts />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>;
+    return <div>
+        <BrowserRouter>
+            <div className='navbar'>
+                <div className='navbar__links'>
+                    <Link to ="/about">about</Link>
+                    <Link to ='/posts'>posts</Link>
+                </div>
+            </div>
+            <Routes>
+                <Route path='/about' element={<About />}>
+                </Route>
+                <Route path='/posts' element={<Posts />}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </div>;
 }
 
 export default App;
