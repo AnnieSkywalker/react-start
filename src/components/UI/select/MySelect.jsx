9c9ee18a2345cacb560e92ec5a6c38038a18f3d3
Spiridonov <annie.skywalker@yandex.ru>
2024-11-19 21:07:43 +0300

    continue work in style select

--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import cl from './MySelect.module.css'
 const MySelect = function ({options, defaultValue, value, onChange}) {
 
   return(
-    <select value={value} onChange={event => onChange(event.target.value)}>
+    <select className={cl.MySelect} value={value} onChange={event => onChange(event.target.value)}>
         <option disabled value=''>{defaultValue}</option>
         {options.map(option =>
             <option key={option.value} value={option.value}>{option.name}</option>
