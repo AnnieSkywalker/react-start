0a6aacd375cf41397e9a3bfb2649421af808803e
Spiridonov <annie.skywalker@yandex.ru>
2024-11-26 09:38:40 +0300

    formating css file

--- src/utils/pages.js
+++ src/utils/pages.js
@@ -3,10 +3,10 @@ export const getPageCount = (totalCount, limit) => {
 }
 
 export const getPagesArray = (totalPages) => {
-  let result = [];
-  for(let i=0; i< totalPages; i++) {
-    result.push(i+1);
-  }
+    let result = [];
+    for(let i=0; i< totalPages; i++) {
+        result.push(i+1);
+    }
 
   return result;
 }
